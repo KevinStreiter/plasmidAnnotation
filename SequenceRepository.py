09f3535c2c9034ebfed731de39be58e2cb0ccc14
Andreas Ott <leaotspam@gmail.com>
2017-12-18 16:17:56 +0100

    qualifiers and shit

--- SequenceRepository.py
+++ SequenceRepository.py
@@ -1,14 +1,16 @@
 class SequenceRepository:
 
-    def __init__(self, sequence, feature_type, qualifiers):
+    def __init__(self, sequence, feature_type, qualifiers, common_qualifier, qualifier_values):
         self.sequence = sequence
         self.feature_type = feature_type
         self.qualifiers = []
+        self.qualifier_values = []
         for qualifier in qualifiers:
             self.qualifiers.append(qualifier)
         self.count = 1
-
-
+        self.common_qualifier = common_qualifier
+        for qualifier_value in qualifier_values:
+            self.qualifier_values.append(qualifier_value)
 
     def getSequence(self):
         return self.sequence
@@ -29,6 +31,18 @@ class SequenceRepository:
         for qualifier in qualifier_list:
             self.qualifiers.append(qualifier)
 
+    def appendQualifierValues(self,qualifier_value_list):
+        for qualifier_value in qualifier_value_list:
+            self.qualifier_values.append(qualifier_value)
+
+    def setCommonQualifier(self,qualifier):
+        self.common_qualifier = qualifier
+
+    def getQualifierValues(self):
+        return self.qualifier_values
+
+    def getCommonQualifier(self):
+        return self.common_qualifier
 
 
 
--- application.py
+++ application.py
@@ -5,6 +5,8 @@
 
 from Bio import SeqIO
 from SequenceRepository import *
+import csv
+from collections import Counter
 
 vectors = SeqIO.parse("vectors-100.gb", "genbank")
 list_of_sequence = []
@@ -20,12 +22,18 @@ for record in vectors:
                     check = False
                     if len(list_of_sequence) > 0:
                         for sequence in list_of_sequence:
-                            if sequence.getSequence() == record.seq and sequence.getFeature_type() == feature.type:
+                            if sequence.getSequence() == feature.extract(record.seq) and sequence.getFeature_type() == feature.type:
                                 sequence.incrementCount()
+                                for qualifier in feature.qualifiers.keys():
+                                        sequence.appendQualifierValues(feature.qualifiers[qualifier])
                                 sequence.appendQualifiers(feature.qualifiers.keys())
                                 check = True
                     if not check:
-                        new_sequence = SequenceRepository(record.seq, feature.type, feature.qualifiers.keys())
+                        qualifier_values = []
+                        for qualifier in feature.qualifiers.keys():
+                            qualifier_values.append(qualifier)
+                        new_sequence = SequenceRepository(feature.extract(record.seq), feature.type, feature.qualifiers.keys(), '',qualifier_values)
+                        #print(new_sequence.getSequence(), new_sequence.getQualifiers(), new_sequence.getFeature_type())
                         list_of_sequence.append(new_sequence)
 
 note_type = ['promoter', 'oriT', 'rep_origin', 'primer_bind', 'terminator', 'misc_signal','misc_recomb','LTR', 'enhancer', '-35_signal', '-10_signal', 'RBS', 'polyA_signal','sig_peptide','protein_bind', 'misc_binding','mobile_element']
@@ -39,56 +47,70 @@ mobile_element_list = []
 gene_type = ['mRNA','-35_signal', '-10_signal', 'RBS', 'polyA_signal','sig_peptide', 'CDS']
 gene_list = []
 undefined_qualifier_list = []
-count = 0
-for sequence in list_of_sequence:
-    #if sequence.getCount() > 2:
-    if sequence.getFeature_type() in note_type:
-        note_type_bool = False
-        for qualifier in sequence.getQualifiers():
-            if qualifier == 'note':
-                note_type_bool = True
-        if note_type_bool:
-            note_list.append(sequence)
-        else:
-            undefined_qualifier_list.append(sequence)
-    elif sequence.getFeature_type() in product_type:
-        product_type_bool = False
-        for qualifier in sequence.getQualifiers():
-            if qualifier == 'product':
-                product_type_bool = True
-        if product_type_bool:
-            product_list.append(sequence)
-        else:
-            undefined_qualifier_list.append(sequence)
-    elif sequence.getFeature_type() in bound_moiety_type:
-        bound_moiety_bool = False
-        for qualifier in sequence.getQualifiers():
-            if qualifier == 'bound_moiety':
-                bound_moiety_bool = True
-        if bound_moiety_bool:
-            bound_moiety_list.append(sequence)
-        else:
-            undefined_qualifier_list.append(sequence)
-    elif sequence.getFeature_type() in mobile_element_type:
-        mobile_bool = False
-        for qualifier in sequence.getQualifiers():
-            if qualifier == 'mobile':
-                mobile_bool = True
-        if mobile_bool:
-            mobile_element_list.append(sequence)
-        else:
-            undefined_qualifier_list.append(sequence)
-    elif sequence.getFeature_type() in gene_type:
-        gene_bool = False
-        for qualifier in sequence.getQualifiers():
-            if qualifier == 'gene':
-                gene_bool = True
-        if gene_bool:
-            gene_list.append(sequence)
-        else:
-            undefined_qualifier_list.append(sequence)
 
-print("Note-List")
-for sequence in mobile_element_list:
-    print(sequence.getFeature_type())
-print (count)
+for sequence in list_of_sequence:
+    if sequence.getCount() > 2:
+        if sequence.getFeature_type() in note_type:
+            note_type_bool = False
+            for qualifier in sequence.getQualifiers():
+                if qualifier == 'note':
+                    note_type_bool = True
+            if note_type_bool:
+                note_list.append(sequence)
+            else:
+                undefined_qualifier_list.append(sequence)
+        elif sequence.getFeature_type() in product_type:
+            product_type_bool = False
+            for qualifier in sequence.getQualifiers():
+                if qualifier == 'product':
+                    product_type_bool = True
+            if product_type_bool:
+                product_list.append(sequence)
+            else:
+                undefined_qualifier_list.append(sequence)
+        elif sequence.getFeature_type() in bound_moiety_type:
+            bound_moiety_bool = False
+            for qualifier in sequence.getQualifiers():
+                if qualifier == 'bound_moiety':
+                    bound_moiety_bool = True
+            if bound_moiety_bool:
+                bound_moiety_list.append(sequence)
+            else:
+                undefined_qualifier_list.append(sequence)
+        elif sequence.getFeature_type() in mobile_element_type:
+            mobile_bool = False
+            for qualifier in sequence.getQualifiers():
+                if qualifier == 'mobile':
+                    mobile_bool = True
+            if mobile_bool:
+                mobile_element_list.append(sequence)
+            else:
+                undefined_qualifier_list.append(sequence)
+        elif sequence.getFeature_type() in gene_type:
+            gene_bool = False
+            for qualifier in sequence.getQualifiers():
+                if qualifier == 'gene':
+                    gene_bool = True
+            if gene_bool:
+                gene_list.append(sequence)
+            else:
+                undefined_qualifier_list.append(sequence)
+        print(sequence.getQualifierValues())
+        common_qualifier,num_most_qualifier = Counter(sequence.getQualifierValues()).most_common(1)[0]
+        print(common_qualifier)
+        sequence.setCommonQualifier(common_qualifier)
+
+print("---------Create File-----")
+file = csv.writer(open("common_features.csv", "w+"))
+header_keys = ['seq', 'feature_type', 'common_qualifier', 'count']
+file.writerow(header_keys)
+for sequence in list_of_sequence:
+    if sequence.getCount()>2 and len(sequence.getSequence()) >= 4:
+        write_row = list()
+        write_row.append(sequence.getSequence())
+        write_row.append(sequence.getFeature_type())
+        write_row.append(sequence.getCommonQualifier())
+        write_row.append(sequence.getCount())
+        file.writerow(write_row)
+#file.close()
+
